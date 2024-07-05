061b1714bb977f3a664569ff0370aebb926e9159
RafaGiaretta <rafaelgiaretta@ymail.com>
2024-07-05 18:46:28 -0300

    Routes  components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { ContatoComponent } from './contato/contato.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+            NavbarComponent, 
+            FooterComponent, 
+            ContatoComponent,
+            FontAwesomeModule,
+            RouterLink,
+            RouterModule
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: 'feed', component: FeedComponent
+    },
+    {
+        path: 'contato', component: ContatoComponent
+    },
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full' //primeira rota direciona para home
+    },
+
+];
